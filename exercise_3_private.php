cf3f1af1e11922e1a4f8152e179bfa57cde165d2
Klarida <klarida19@gmail.com>
2022-06-24 10:56:57 +0200

    ex4

--- exercise_3_private.php
+++ exercise_3_private.php
@@ -3,62 +3,84 @@
 declare(strict_types=1);
 
 /* EXERCISE 3
-
 TODO: Copy the code of exercise 2 to here and delete everything related to cola.
 TODO: Make all properties private.
 TODO: Make all the other prints work without error.
 TODO: After fixing the errors. Change the color of Duvel to light instead of blond and also print this new color on the screen after all the other things that were already printed (to be sure that the color has changed).
 TODO: Create a new private method in the Beer class called beerInfo which returns "Hi i'm Duvel and have an alcochol percentage of 8.5 and I have a light color."
-
 Make sure that you use the variables and not just this text line.
-
 TODO: Print this method on the screen on a new line.
-
 USE TYPEHINTING EVERYWHERE!
 */
-class Beverage
-{
-// get properties, only public in this case
+class beverage{
     private string $color;
     private float $price;
     private string $temperature;
-    public function __construct(string $color, float $price)
-    {
+
+    public function __construct(string $color, float $price){
         $this->color = $color;
         $this->price = $price;
-        $this->temperature = 'cold';
-
+        $this->temperature = "cold";
     }
 
-    public function getinfo()
+    public function getInfo() : void{
+        echo "This beverage is $this->temperature and $this->color and the price is &euro; $this->price. ";
+    }
+
+    /**
+     * @return string
+     */
+    public function getColor(): string
     {
-        echo "This beverage is $this->color and $this->temperature ";
+        return $this->color;
+    }
+
+    /**
+     * @param string $color
+     */
+    public function setColor(string $color): void
+    {
+        $this->color = $color;
     }
 
 }
 
-class beer extends Beverage{
-// get properties, only public in this case
-    private string  $name;
-    private float   $alocoholPercentage;
-    private  function __construct(string $color, float $price , string $name, float $alocoholPercentage)
+class beer extends beverage{
+    private string $name;
+    private float $alcoholPercentage;
+
+    /**
+     * @param string $name
+     * @param float $alcoholPercentage
+     */
+    public function __construct(string $name, float $alcoholPercentage, string $color, float $price)
     {
-        parent::__construct($color, $price);
-        $this->name =$name;
-        $this->alocoholPercentage =$alocoholPercentage;
+        parent::__construct($color , $price);
+        $this->name = $name;
+        $this->alcoholPercentage = $alcoholPercentage;
     }
 
     /**
      * @return float
      */
-    public function getAlocoholPercentage(): float
+    public function getAlcoholPercentage(): float
     {
-        return $this->alocoholPercentage;
+        return $this->alcoholPercentage;
     }
 
+    /**
+     * @return string
+     */
+    public function beerInfo(): string{
+        return "Hello I'm $this->name and have an alcochol percentage of $this->alcoholPercentage and I have a". $this->getColor() ."color";
+    }
 }
-$duvel = new beer('blond', 3.5, 'duvel',8.5);
-echo  $duvel ->alocoholPercentage;
-echo  $duvel ->getAlocoholPercentage();
-echo  $duvel ->color;
-echo  $duvel->getinfo();
+$duvel = new beer('Duvel', 8.7, 'blond',3.5);
+echo $duvel->getAlcoholPercentage().'<br>';
+//echo $duvel->alcoholPercentage.'<br>';
+echo $duvel->getInfo().'<br>';
+echo $duvel->getColor().'<br>';
+$duvel->setColor('light');
+echo "color changed to: <br>";
+echo $duvel->getColor() . '<br>';
+echo $duvel->beerInfo();
--- exercise_4_protected.php
+++ exercise_4_protected.php
@@ -11,3 +11,51 @@ TODO: Make all the other prints work without error without changing the beverage
 
 USE TYPEHINTING EVERYWHERE!
 */
+class Beverage
+{
+// get properties, only public in this case
+    protected string $color;
+    protected float $price;
+    protected string $temperature;
+    public function __construct(string $color, float $price)
+    {
+        $this->color = $color;
+        $this->price = $price;
+        $this->temperature = 'cold';
+
+    }
+
+    public function getinfo()
+    {
+        echo "This beverage is $this->color and $this->temperature ";
+    }
+
+}
+
+class beer extends Beverage{
+// get properties, only public in this case
+protected string  $name;
+protected float   $alocoholPercentage;
+
+
+public  function __construct(string $color, float $price , string $name, float $alocoholPercentage)
+    {
+        parent::__construct($color, $price);
+        $this->name =$name;
+        $this->alocoholPercentage =$alocoholPercentage;
+    }
+
+    /**
+     * @return float
+     */
+    public function getAlocoholPercentage(): float
+    {
+        return $this->alocoholPercentage;
+    }
+
+}
+$duvel = new beer('blond', 3.5, 'duvel',8.5);
+echo  $duvel ->alocoholPercentage;
+echo  $duvel ->getAlocoholPercentage();
+echo  $duvel ->color;
+echo  $duvel->getinfo();
