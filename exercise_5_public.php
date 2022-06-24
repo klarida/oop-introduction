b2503b2eb19aa8bf5061f67cf3a4e70cf980b6be
Klarida <klarida19@gmail.com>
2022-06-24 12:01:49 +0200

    ex 5

--- exercise_5_public.php
+++ exercise_5_public.php
@@ -10,3 +10,38 @@ TODO: Change the properties to private.
 TODO: Fix the errors without using getter and setter functions.
 TODO: Change the price to 3.5 euro and print it also on the screen on a new line.
 */
+class Beverage
+{
+
+    private string $color;
+    private float $price;
+    private string $temperature;
+
+    public function __construct(string $color, float $price)
+    {
+        $this->color = $color;
+        $this->price = $price;
+        $this->temperature = 'cold';
+
+    }
+    public function getinfo(){
+        echo "This beverage is $this->color and $this->temperature ";
+    }
+    public function IsTemperature() : string{
+        return "the temperature is : $this->temperature <br>";
+    }
+    public function changedPrice(float $price) : string{
+        if ($price > 0){
+            return 'the new price is: '.$this->price = $price;
+        }
+        return 'price can\'t be negetive or 0!';
+    }
+
+}
+
+$cola = new beverage( 'black', 2);
+echo $cola->getInfo().'<br>';
+echo $cola->IsTemperature();
+echo $cola->changedPrice(3.5);
+
+
